0ed4ec6fb2e643438ddb6db37d1bf48e070ce699
Felipe Batista <fmelobatista@gmail.com>
2018-09-13 11:10:21 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/felipebatista/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.felipebatista.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.felipebatista.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+
+}
