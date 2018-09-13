03be8bc69f087279cb1eadfe49609d33effe057e
Felipe Batista <fmelobatista@gmail.com>
2018-09-13 13:16:50 -0300

    Inclusao de cidade e estado Repositorio e Classes de Domain

--- src/main/java/com/felipebatista/cursomc/CursomcApplication.java
+++ src/main/java/com/felipebatista/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.felipebatista.cursomc.domain.Categoria;
+import com.felipebatista.cursomc.domain.Cidade;
+import com.felipebatista.cursomc.domain.Estado;
 import com.felipebatista.cursomc.domain.Produto;
 import com.felipebatista.cursomc.repositories.CategoriaRepository;
+import com.felipebatista.cursomc.repositories.CidadeRepository;
+import com.felipebatista.cursomc.repositories.EstadoRepository;
 import com.felipebatista.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private CategoriaRepository categoriaRepo;
 	@Autowired
 	private ProdutoRepository produtoRepo;
+	@Autowired
+	private CidadeRepository cidadeRepo;
+	@Autowired
+	private EstadoRepository estadoRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -43,8 +51,25 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		
+		Cidade c1 = new Cidade(null,"Uberlandia", est1);
+		Cidade c2 = new Cidade(null,"São Paulo", est2);
+		Cidade c3 = new Cidade(null,"Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		
 		categoriaRepo.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepo.saveAll(Arrays.asList(p1,p2,p3));
+		
+		//Salvar primeiro estados pq ele está incluido na relação com cidades
+		estadoRepo.saveAll(Arrays.asList(est1,est2));
+		cidadeRepo.saveAll(Arrays.asList(c1,c2,c3));
 		
 		
 		
---
+++ src/main/java/com/felipebatista/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,12 @@
+package com.felipebatista.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.felipebatista.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+	
+
+}
